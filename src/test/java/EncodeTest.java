0ba6eb101e68ea9e19d76cd618d36a563aa2c0b4
cliserkad <bryanchristopherjohnson@gmail.com>
2020-11-04 11:59:34 -0500

    Add ParseTest. Increment version

--- src/test/java/EncodeTest.java
+++ src/test/java/EncodeTest.java
@@ -1,17 +1,16 @@
 package test.java;
 
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
 import static com.xarql.smp.ToSMP.encode;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EncodeTest {
 
-	public static final String EXPECTED = "{year:1998;model:\"Honda Accord\";engine:{cylinders:6;name:\"VTEC\";};}";
+	public static final String EXPECTED = "{year:1998;model:\"Honda Accord\";engine:{cylinders:6;name:\"VTEC\";mpg:14.834;};type:'c';}";
 
 	@Test
-	public void testCar() throws IllegalAccessException, IOException {
-		assertEquals(encode(new Car()), EXPECTED);
+	public void testCar() throws IllegalAccessException {
+		assertEquals(EXPECTED, encode(new Car()));
 	}
 
 }
---
+++ src/test/java/ParseTest.java
@@ -0,0 +1,32 @@
+package test.java;
+
+import com.xarql.smp.StringPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static com.xarql.smp.GenericParser.parse;
+import static com.xarql.smp.ToSMP.encode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParseTest {
+
+    public static final int EXPECTED_YEAR = 1998;
+    public static final String EXPECTED_MODEL = "Honda Accord";
+    public static final char EXPECTED_TYPE = 'c';
+    public static final int EXPECTED_ENGINE_CYLINDERS = 6;
+    public static final String EXPECTED_ENGINE_NAME = "VTEC";
+    public static final float EXPECTED_ENGINE_MPG = 14.834f;
+
+    @Test
+    public void testCar() throws IllegalAccessException {
+        final Map<StringPath, Object> data = parse(encode(new Car()));
+        assertEquals(EXPECTED_YEAR, data.get(new StringPath("year")));
+        assertEquals(EXPECTED_MODEL, data.get(new StringPath("model")));
+        assertEquals(EXPECTED_TYPE, data.get(new StringPath("type")));
+        assertEquals(EXPECTED_ENGINE_CYLINDERS, data.get(new StringPath("engine", "cylinders")));
+        assertEquals(EXPECTED_ENGINE_NAME, data.get(new StringPath("engine", "name")));
+        assertEquals(EXPECTED_ENGINE_MPG, data.get(new StringPath("engine", "mpg")));
+    }
+
+}
