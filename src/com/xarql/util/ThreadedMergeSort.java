2db818e2959c287f343d7089f4e96749795ce20f
cliserkad <cliserkad@xarql.com>
2022-05-03 23:15:12 -0400

    prematurely optimize ThreadedMergeSort

--- src/com/xarql/util/ThreadedMergeSort.java
+++ src/com/xarql/util/ThreadedMergeSort.java
@@ -6,43 +6,51 @@ public class ThreadedMergeSort {
 
 	}
 
-	public static void mergeSort(int[] array) {
-		if (array.length < 2) {
+	public static void mergeSort(int[] source) {
+		// if array length is 2
+		if(source.length == 2) {
+			// if indexes 1 & 2 are not sorted
+			if(source[0] > source[1]) {
+				// swap indexes 1 & 2
+				int tmp = source[0];
+				source[0] = source[1];
+				source[1] = tmp;
+			}
 			return;
 		}
-		int mid = array.length / 2;
-		int[] l = new int[mid];
-		int[] r = new int[array.length - mid];
+		int mid = source.length / 2;
 
-		for (int i = 0; i < mid; i++) {
-			l[i] = array[i];
-		}
-		for (int i = mid; i < array.length; i++) {
-			r[i - mid] = array[i];
-		}
-		mergeSort(l);
-		mergeSort(r);
+		int[] leftPart = new int[mid];
+		System.arraycopy(source, 0, leftPart, 0, mid);
+		// only sort if array has more than 1 element
+		if(leftPart.length > 1)
+			mergeSort(leftPart);
 
-		merge(array, l, r, mid, array.length - mid);
+		int[] rightPart = new int[source.length - mid];
+		System.arraycopy(source, mid, rightPart, 0, source.length - mid);
+		// only sort if array has more than 1 element
+		if(rightPart.length > 1)
+			mergeSort(rightPart);
+
+		merge(source, leftPart, rightPart, mid, source.length - mid);
 	}
 
-	public static void merge(int[] a, int[] l, int[] r, int left, int right) {
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		while (i < left && j < right) {
-			if (l[i] <= r[j]) {
-				a[k++] = l[i++];
-			}
-			else {
-				a[k++] = r[j++];
+	public static void merge(int[] source, int[] leftPart, int[] rightPart, int left, int right) {
+		int leftCounter = 0;
+		int rightCounter = 0;
+		int destinationCounter = 0;
+		while(leftCounter < left && rightCounter < right) {
+			if(leftPart[leftCounter] <= rightPart[rightCounter]) {
+				source[destinationCounter++] = leftPart[leftCounter++];
+			} else {
+				source[destinationCounter++] = rightPart[rightCounter++];
 			}
 		}
-		while (i < left) {
-			a[k++] = l[i++];
+		while(leftCounter < left) {
+			source[destinationCounter++] = leftPart[leftCounter++];
 		}
-		while (j < right) {
-			a[k++] = r[j++];
+		while(rightCounter < right) {
+			source[destinationCounter++] = rightPart[rightCounter++];
 		}
 	}
 
--- src/test/java/ThreadedMergeSortTest.java
+++ src/test/java/ThreadedMergeSortTest.java
@@ -3,16 +3,29 @@ package test.java;
 import com.xarql.util.ThreadedMergeSort;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class ThreadedMergeSortTest {
 
+	// sort 10 million numbers
 	@Test
 	public void testArray() {
-		int[] actual = { 5, 1, 6, 2, 3, 4 };
-		int[] expected = { 1, 2, 3, 4, 5, 6 };
-		ThreadedMergeSort.mergeSort(actual);
-		assertArrayEquals(expected, actual);
+		Random r = new Random();
+		int[] randomized = new int[10000000];
+		for(int i = 0; i < randomized.length; i++) {
+			randomized[i] = r.nextInt(10000);
+		}
+		int[] sorted = new int[randomized.length];
+		System.arraycopy(randomized, 0, sorted, 0, randomized.length);
+		Arrays.sort(sorted);
+
+		ThreadedMergeSort.mergeSort(randomized);
+		assertArrayEquals(randomized, sorted);
 	}
+
+
 
 }
