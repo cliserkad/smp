95727fc6a6c879498dca0283d6207daf81f8938a
cliserkad <0_0@cliserkad.xyz>
2024-05-09 03:02:20 -0400

    make MergeSort generic

--- src/com/xarql/util/MergeSort.java
+++ src/com/xarql/util/MergeSort.java
@@ -2,26 +2,27 @@ package com.xarql.util;
 
 import java.util.concurrent.RecursiveTask;
 
-public class MergeSort extends RecursiveTask<int[]> {
+/* black magic that already has a standard library implementation */
+public class MergeSort<T> extends RecursiveTask<Comparable<T>[]> {
 	public static final int INSERTION_SORT_THRESHOLD = 256;
 
-	private final int[] array;
+	private final Comparable<T>[] array;
 
-	public MergeSort(int[] array) {
+	public MergeSort(final Comparable<T>[] array) {
 		this.array = array;
 	}
 
-	public static int[] sort(int[] array) {
-		MergeSort sorter = new MergeSort(array);
+	public static <T> Comparable<T>[] sort(Comparable<T>[] array) {
+		MergeSort<T> sorter = new MergeSort<>(array);
 		return sorter.compute();
 	}
 
-	public static void merge(int[] source, int[] leftPart, int[] rightPart) {
+	public static <T> void merge(Comparable<T>[] source, Comparable<T>[] leftPart, Comparable<T>[] rightPart) {
 		int leftIndex = 0;
 		int rightIndex = 0;
 		int sourceIndex = 0;
 		while(leftIndex < leftPart.length && rightIndex < rightPart.length) {
-			if(leftPart[leftIndex] < rightPart[rightIndex]) {
+			if(leftPart[leftIndex].compareTo((T) rightPart[rightIndex]) <= 0) {
 				source[sourceIndex++] = leftPart[leftIndex++];
 			} else {
 				source[sourceIndex++] = rightPart[rightIndex++];
@@ -35,27 +36,28 @@ public class MergeSort extends RecursiveTask<int[]> {
 		}
 	}
 
-	public static int[] insertionSort(int[] array) {
-		for(int a = 1; a < array.length; ++a) {
+	public static <T> Comparable<T>[] insertionSort(Comparable<T>[] array) {
+		for(int keyIndex = 1; keyIndex < array.length; ++keyIndex) {
 			// key to test targets against
-			int key = array[a];
+			Comparable<T> key = array[keyIndex];
 			// index to target for insertion
-			int target = a - 1;
+			int targetIndex = keyIndex - 1;
 
-            /* Move elements of array[0..i-1], that are
+            /* Move elements of array[0..keyIndex-1], that are
                greater than key, to one position ahead
                of their current position */
-			while(target >= 0 && array[target] > key) {
-				array[target + 1] = array[target];
-				target--;
+			/* unchecked cast here, I'm purposefully letting this error out if the elements are incompatible */
+			while(targetIndex >= 0 && array[targetIndex].compareTo((T) key) > 0) {
+				array[targetIndex + 1] = array[targetIndex];
+				targetIndex--;
 			}
-			array[target + 1] = key;
+			array[targetIndex + 1] = key;
 		}
 		return array;
 	}
 
 	@Override
-	protected int[] compute() {
+	protected Comparable<T>[] compute() {
 		// when the array is too small, creating threads isn't worth the overhead
 		// this also serves as the base sorting algorithm, instead of a simple 2 element swap
 		if(array.length < INSERTION_SORT_THRESHOLD) {
@@ -65,14 +67,14 @@ public class MergeSort extends RecursiveTask<int[]> {
 		int mid = array.length / 2;
 
 		// numbers to the left of the mid point
-		int[] leftPart = new int[mid];
+		Comparable<T>[] leftPart = new Comparable[mid];
 		System.arraycopy(array, 0, leftPart, 0, mid);
-		MergeSort leftSorter = new MergeSort(leftPart);
+		MergeSort<T> leftSorter = new MergeSort<>(leftPart);
 
 		// numbers to the right of the mid point
-		int[] rightPart = new int[array.length - mid];
+		Comparable<T>[] rightPart = new Comparable[array.length - mid];
 		System.arraycopy(array, mid, rightPart, 0, array.length - mid);
-		MergeSort rightSorter = new MergeSort(rightPart);
+		MergeSort<T> rightSorter = new MergeSort<>(rightPart);
 
 		// dispatch threads
 		leftSorter.fork();
